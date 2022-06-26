45ccd9797d2b9eb679a1f7537f3c18c9fa5ffd73
aslanshaken <aslanshaken@gmail.com>
2022-06-26 16:03:07 -0400

    fundaments

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return(
+  <div className="App">
+    Aslan Shaken
+    {/* Home */}
+    {/* About */}
+    {/* Work Experiance */}
+    {/* Projects */}
+    {/* Skills */}
+    {/* Resume */}
+  </div>
+  );
+}
+
+export default App;
