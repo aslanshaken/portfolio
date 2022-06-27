482f019ad376ad287f8739e51fba1a71bc41c8c5
aslanshaken <aslanshaken@gmail.com>
2022-06-26 21:11:28 -0400

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Home from "./pages/Home/Home";
+import Header from "./pages/Header/Header";
+import TextCarousel from "./pages/TextCarousel/TextCarousel"
 import './App.css'
 
 function App() {
   return(
   <div className="App">
-    <Home/>
+    <Header/>
+    <TextCarousel/>
     {/* About */}
     {/* Work Experiance */}
     {/* Projects */}
---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import React from "react";
+
+function Header() {
+  return (
+    <Navbar expand="xl" variant="light" sticky="top" className="gap-3 px-3">
+      <Container className="align-items:center">
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="flex-grow-1 justify-content-center align-items-center">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Work Experience</Nav.Link>
+            <Nav.Link href="#home">Projects</Nav.Link>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">Resume</Nav.Link>
+            <Nav.Link>
+              <Button variant="primary">Contact Me</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
