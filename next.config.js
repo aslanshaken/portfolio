9edbb1e90bc8842aa471eb8688311afef6fd0f97
Elite <petpanda0057@gmail.com>
2023-03-20 16:26:48 +0200

    feat: add Google login functionality not perfect

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ module.exports = withTM({
     APP_NAME: 'Hickory - United Food',
     // HOST
     HOST_API_KEY: 'https://96a8-175-101-156-41.in.ngrok.io',
-    API_VERSION: 'v1'
+    API_VERSION: 'v1',
+    // GOOGLE
+    GOOGLE_CLIENT_ID: '688314916557-etc1cai7htim0hk0hlc26853cscmk3bf.apps.googleusercontent.com'
   },
 });
