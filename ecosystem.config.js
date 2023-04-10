18b3ab582019c1086f97731df5cdbe1b03d2b4cb
Chaitanya <prabhuchaitanyavarma@gmail.com>
2023-04-10 11:30:34 +0530

    deployment setup

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "cookk-front-end-app",
+      env: {
+        PORT: 3000,
+        NODE_ENV: "production"
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-34-238-53-251.compute-1.amazonaws.com",
+      key: "cookk-frontend-ssh.pem",
+      ref: "origin/dev",
+      repo: "git@github.com:UnitedFood/front-end.git",
+      path: "/home/ubuntu/front-end-app",
+      "post-deploy":
+        "npm install && npm run build && sudo cp -r build/* /var/www/cookk.co/"
+    }
+  }
+};
