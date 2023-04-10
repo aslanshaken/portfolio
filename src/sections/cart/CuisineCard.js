0db579936aea6f39d37e00dee3783632b13b0f18
Elite <petpanda0057@gmail.com>
2023-04-10 22:24:09 +0300

    feat: update cuisine card spacing in cart page

--- src/sections/cart/CuisineCard.js
+++ src/sections/cart/CuisineCard.js
@@ -23,33 +23,37 @@ export default function CuisineCard({ cuisine = {} }) {
       width={'100%'}
       spacing={{ xs: 2, md: 5, lg: 7 }}
     >
-      <Image
-        alt={cuisine?.title}
-        src={cuisine?.image_url}
-        width={'80px'}
-        height={'80px'}
-        sx={{ borderRadius: '50%', minWidth: 80, minHeight: 80 }}
-      />
+      <Stack direction={'row'} alignItems={'center'} spacing={4} minWidth={300}>
+        <Image
+          alt={cuisine?.title}
+          src={cuisine?.image_url}
+          width={'80px'}
+          height={'80px'}
+          sx={{ borderRadius: '50%', minWidth: 80, minHeight: 80 }}
+        />
 
-      <Stack>
-        <Typography variant="h6" color="black" fontWeight={600} gutterBottom>
-          {cuisine?.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {'440 cal'}
-        </Typography>
+        <Stack>
+          <Typography variant="h6" color="black" fontWeight={600} gutterBottom>
+            {cuisine?.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {'440 cal'}
+          </Typography>
+        </Stack>
       </Stack>
 
-      <Typography color="text.secondary">{cuisine?.count}</Typography>
+      <Stack direction={'row'} alignItems={'center'} spacing={4}>
+        <Typography color="text.secondary">{cuisine?.count}</Typography>
 
-      <IconButtonAnimate
-        color="error"
-        onClick={() => {
-          dispatch(removeFoodCart({ food: cuisine, removeAll: false, removeOneItem: false }));
-        }}
-      >
-        <Iconify icon={'mdi:trash'} />
-      </IconButtonAnimate>
+        <IconButtonAnimate
+          color="error"
+          onClick={() => {
+            dispatch(removeFoodCart({ food: cuisine, removeAll: false, removeOneItem: false }));
+          }}
+        >
+          <Iconify icon={'mdi:trash'} />
+        </IconButtonAnimate>
+      </Stack>
     </Stack>
   );
 }
