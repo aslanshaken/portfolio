c5399f4212350bf356b28a28cd3f7ab6bf776c6f
aslanshaken <aslanshaken@gmail.com>
2024-08-06 15:37:42 -0500

    small css change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <Link to='/' className="header__title"><h2>Portfolio</h2></Link>
+      <Link to='/' className="header__title"><h4>Portfolio</h4></Link>
       <div className='header__links'>
         <Link to='/education' className="header__link"><h4>Education</h4></Link>
         <Link to='/work' className="header__link"><h4>Work</h4></Link>
